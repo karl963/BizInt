71466f7043537632c5864916d7789805d7114fe3
Karl963 <Karl@192.168.1.74>
2013-09-18 20:10:18 +0300

    teine

---
+++ BizInt/test/bizint/web/HelloControllerTests.java
@@ -0,0 +1,17 @@
+package bizint.web;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import bizint.web.HelloController;
+
+import junit.framework.TestCase;
+
+public class HelloControllerTests extends TestCase {
+
+    public void testHandleRequestView() throws Exception{		
+        HelloController controller = new HelloController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);		
+        assertEquals("hello.jsp", modelAndView.getViewName());
+    }
+}
+
